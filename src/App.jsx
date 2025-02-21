29d676126c078f331dca580279bcec13f0531623
akash2144 <ghoshbabu2144@gmail.com>
2025-02-21 23:46:00 +0600

    here i added some animation on banner section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     
-      <h1>Vite + React</h1>
+      <h1>Job Portal</h1>
       
     </>
   )
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,21 +1,34 @@
 import { easeOut } from "motion";
 import * as motion from "motion/react-client"
+import team1 from "../../assets/pictures/team4.jpg";
+import team2 from "../../assets/pictures/team5.jpg";
+import team3 from "../../assets/pictures/team3.jpg";
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-96">
+        <div className="hero bg-base-200 min-h-120">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="flex-1">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
+    <motion.img
+      src={team1}
+      animate={{y:[50,100,50]}}
+      transition={{duration:10,repeat:Infinity}}
+      className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-700 shadow-2xl" />
+    <motion.img
+      src={team2}
+      animate={{x:[100,150,100]}}
+      transition={{duration:10,repeat:Infinity}}
+      className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-700 shadow-2xl" />
     </div>
     <div className="flex-1">
       <motion.h1 
       animate={{x:50}}
       transition={{duration:2, delay:1, ease:easeOut, repeat:Infinity}}
       
-      className="text-5xl font-bold">Lates Jobs For You!</motion.h1>
+      className="text-5xl font-bold">Lates <motion.span
+      animate={{color:['#33ddff','#33ff9c','#f0ff33']}}
+      transition={{duration:1.5, repeat:Infinity}}
+      >Jobs</motion.span> For You!</motion.h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
